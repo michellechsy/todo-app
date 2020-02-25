ee4e5f84c038b56ff0016e7a5ae8f5d156c301a6
Michelle <michelle.chsy@gmail.com>
2020-02-25 23:25:58 +0800

    ci: #1 set up travis ci & update db migrate script (#10)

--- packages/db/migrations/20200216150802-create-user.js
+++ packages/db/migrations/20200216150802-create-user.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const uuidv4 = require('uuid/v4')
 const uuidv5 = require('uuid/v5')
 
@@ -7,46 +5,45 @@ const name = 'users'
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    if (queryInterface.has)
-      return queryInterface.createTable(name, {
-        id: {
-          type: Sequelize.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-          allowNull: false
-        },
-        username: Sequelize.STRING,
-        password: Sequelize.STRING,
-        token: {
-          type: Sequelize.UUID,
-          unique: true,
-          allowNull: true,
-          defaultValue() {
-            return uuidv5(uuidv4(), uuidv5('users', uuidv5.DNS))
-          }
-        },
-        profile: {
-          type: Sequelize.TEXT('long'),
-          allowNull: true
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue() {
-            return Date.now()
-          }
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue() {
-            return Date.now()
-          }
+    return queryInterface.createTable(name, {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      username: Sequelize.STRING,
+      password: Sequelize.STRING,
+      token: {
+        type: Sequelize.UUID,
+        unique: true,
+        allowNull: true,
+        defaultValue() {
+          return uuidv5(uuidv4(), uuidv5(name, uuidv5.DNS))
         }
-      })
+      },
+      profile: {
+        type: Sequelize.TEXT('long'),
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue() {
+          return Date.now()
+        }
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue() {
+          return Date.now()
+        }
+      }
+    })
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable(name);
+  down: queryInterface => {
+    return queryInterface.dropTable(name)
   }
-};
+}
