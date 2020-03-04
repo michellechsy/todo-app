11d8cc7c9cfeb6f6c0bd1aa52a5c0818982e79a8
Michelle <michelle.chsy@gmail.com>
2020-03-04 21:52:24 +0800

    chore: #3 add semantic release for changelog
    * chore: #3 add semantic release for changelog
    
    * chore: #3 update release version and yarn script for release

---
+++ release.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  branch: 'master',
+  tagFormat: 'v${version}', // eslint-disable-line no-template-curly-in-string
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'feat', release: 'minor' }
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES']
+        }
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        changelogFile: 'docs/CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/github',
+      {
+        githubUrl: 'https://github.com/michellechsy/todo-app.git'
+      }
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['docs/CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}' // eslint-disable-line no-template-curly-in-string
+      }
+    ]
+  ]
+}
